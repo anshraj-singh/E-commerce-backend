b43374dab1fc8b39ee2b800e5bd550972e83dea0
ansh raj <anshrajsingh6565555@gmail.com>
2026-01-13 19:56:40 +0530

    fix product apis for admin can do

--- src/main/java/com/quickcart/ecommerce/controller/AdminController.java
+++ src/main/java/com/quickcart/ecommerce/controller/AdminController.java
@@ -1,8 +1,10 @@
 package com.quickcart.ecommerce.controller;
 
 import com.quickcart.ecommerce.entity.Order;
+import com.quickcart.ecommerce.entity.Product;
 import com.quickcart.ecommerce.entity.UserEntry;
 import com.quickcart.ecommerce.service.OrderService;
+import com.quickcart.ecommerce.service.ProductService;
 import com.quickcart.ecommerce.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/admin")
@@ -20,6 +23,9 @@ public class AdminController {
 
     @Autowired
     private OrderService orderService;
+
+    @Autowired
+    private ProductService productService;
 
     @GetMapping("/getAllUsers")
     public ResponseEntity<List<UserEntry>> getAllUsers() {
@@ -51,6 +57,45 @@ public class AdminController {
         List<Order> allOrders = orderService.getAllOrders();
         if (!allOrders.isEmpty()) {
             return new ResponseEntity<>(allOrders, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/createProduct")
+    public ResponseEntity<?> createProduct(@RequestBody Product newProduct) {
+        try {
+            productService.saveProduct(newProduct);
+            return new ResponseEntity<>(newProduct, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/id/{productId}")
+    public ResponseEntity<Void> deleteProductById(@PathVariable String productId) {
+        productService.deleteById(productId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("/id/{productId}")
+    public ResponseEntity<Product> updateProductById(@PathVariable String productId, @RequestBody Product updatedProduct) {
+        Optional<Product> existingProductOpt = productService.getById(productId);
+        if (existingProductOpt.isPresent()) {
+            Product existingProduct = existingProductOpt.get();
+            if (updatedProduct.getName() != null && !updatedProduct.getName().isEmpty()) {
+                existingProduct.setName(updatedProduct.getName());
+            }
+            if (updatedProduct.getDescription() != null && !updatedProduct.getDescription().isEmpty()) {
+                existingProduct.setDescription(updatedProduct.getDescription());
+            }
+            if (updatedProduct.getPrice() >= 0) {
+                existingProduct.setPrice(updatedProduct.getPrice());
+            }
+            if (updatedProduct.getStock() >= 0) {
+                existingProduct.setStock(updatedProduct.getStock());
+            }
+            productService.saveProduct(existingProduct);
+            return new ResponseEntity<>(existingProduct, HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
--- src/main/java/com/quickcart/ecommerce/controller/ProductController.java
+++ src/main/java/com/quickcart/ecommerce/controller/ProductController.java
@@ -26,50 +26,11 @@ public class ProductController {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @PostMapping("/createProduct")
-    public ResponseEntity<?> createProduct(@RequestBody Product newProduct) {
-        try {
-            productService.saveProduct(newProduct);
-            return new ResponseEntity<>(newProduct, HttpStatus.CREATED);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-    }
-
     @GetMapping("/id/{productId}")
     public ResponseEntity<Product> getProductById(@PathVariable String productId) {
         Optional<Product> product = productService.getById(productId);
         if (product.isPresent()) {
             return new ResponseEntity<>(product.get(), HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    }
-
-    @DeleteMapping("/id/{productId}")
-    public ResponseEntity<Void> deleteProductById(@PathVariable String productId) {
-        productService.deleteById(productId);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
-
-    @PutMapping("/id/{productId}")
-    public ResponseEntity<Product> updateProductById(@PathVariable String productId, @RequestBody Product updatedProduct) {
-        Optional<Product> existingProductOpt = productService.getById(productId);
-        if (existingProductOpt.isPresent()) {
-            Product existingProduct = existingProductOpt.get();
-            if (updatedProduct.getName() != null && !updatedProduct.getName().isEmpty()) {
-                existingProduct.setName(updatedProduct.getName());
-            }
-            if (updatedProduct.getDescription() != null && !updatedProduct.getDescription().isEmpty()) {
-                existingProduct.setDescription(updatedProduct.getDescription());
-            }
-            if (updatedProduct.getPrice() >= 0) {
-                existingProduct.setPrice(updatedProduct.getPrice());
-            }
-            if (updatedProduct.getStock() >= 0) {
-                existingProduct.setStock(updatedProduct.getStock());
-            }
-            productService.saveProduct(existingProduct);
-            return new ResponseEntity<>(existingProduct, HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
